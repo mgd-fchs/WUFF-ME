d184d07ac3fd81c6bcff3cbdf5e7db1b99f9a71e
Thomas Pinheiro de Souza <pinheirodesouza@student.tugraz.at>
2022-05-11 09:40:22 +0200

    WUFF-2 RED Add UsertypeSelectionActivity test to see if all ui elements are present [FB, TP]

---
+++ app/src/main/java/at/tugraz/software22/ui/UsertypeSelectionActivity.java
@@ -0,0 +1,16 @@
+package at.tugraz.software22.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import at.tugraz.software22.R;
+
+public class UsertypeSelectionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.usertype_selection);
+    }
+}
