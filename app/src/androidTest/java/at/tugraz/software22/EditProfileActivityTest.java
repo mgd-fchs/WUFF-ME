1dd37c2db78c578a76ba09df61ca9710cdcc488c
Michael Baier <baier@student.tugraz.at>
2022-05-11 12:36:54 +0200

    WUFF-3 RED Add a testcase for birthday changes [JS, MB]

--- app/src/androidTest/java/at/tugraz/software22/EditProfileActivityTest.java
+++ app/src/androidTest/java/at/tugraz/software22/EditProfileActivityTest.java
@@ -4,9 +4,12 @@ import android.content.res.Resources;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.ViewAssertion;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.base.RootViewPicker;
+import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -16,6 +19,10 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import at.tugraz.software22.domain.entity.User;
 import at.tugraz.software22.domain.repository.UserRepository;
@@ -41,16 +48,16 @@ public class EditProfileActivityTest {
 
     @Test
     public void givenLoggedInUser_whenActivityStarted_thenVerifyThatUserNameIsDisplayed(){
-        User user = new User("Testuser");
+        User user = new User("Testuser",  LocalDate.now());
         Mockito.when(userRepositoryMock.getLoggedInUser()).thenReturn(user);
         ActivityScenario.launch(EditProfileActivity.class);
         Espresso.onView(ViewMatchers.withText(user.getUsername())).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
 
     @Test
-    public void givenLoggedInUser_whenNameEditedAndSubmitted_thenVerifyThatUpdateUserIsCalled() {
+    public void givenLoggedInUser_whenNameEditedAndSubmitted_thenVerifyThatUpdateUserIsCalledWithCorrectName() {
         String newUserName = "New Name";
-        Mockito.when(userRepositoryMock.getLoggedInUser()).thenReturn(new User("Testuser"));
+        Mockito.when(userRepositoryMock.getLoggedInUser()).thenReturn(new User("Testuser", LocalDate.now()));
         ActivityScenario.launch(EditProfileActivity.class);
 
         Espresso.onView(ViewMatchers.withId(R.id.imageButtonEditUserName))
@@ -63,6 +70,25 @@ public class EditProfileActivityTest {
                 .perform(ViewActions.click());
 
         Mockito.verify(userRepositoryMock).updateUser(Mockito.argThat(user -> user.getUsername().equals(newUserName)) );
+    }
+
+    @Test
+    public void givenLoggedInUser_whenAgeEditedAndSubmitted_thenVerifyThatUpdateUserIsCalledWithCorrectAge() {
+        LocalDate newUserBirthday = LocalDate.of(2000,11,3);
+        Mockito.when(userRepositoryMock.getLoggedInUser()).thenReturn(new User("Testuser", LocalDate.now()));
+        ActivityScenario.launch(EditProfileActivity.class);
+
+        Espresso.onView(ViewMatchers.withId(R.id.imageButtonEditAge))
+                .perform(ViewActions.click());
+
+        Espresso.onView(ViewMatchers.withId(R.id.editTextAge))
+                .perform(ViewActions.typeText(newUserBirthday.format(DateTimeFormatter.ISO_DATE)));
+
+
+        Espresso.onView(ViewMatchers.withId(R.id.imageButtonEditAge))
+                .perform(ViewActions.click());
+
+        Mockito.verify(userRepositoryMock).updateUser(Mockito.argThat(user -> user.getBirthday().equals(newUserBirthday)) );
     }
 
 }
--- app/src/main/java/at/tugraz/software22/domain/entity/User.java
+++ app/src/main/java/at/tugraz/software22/domain/entity/User.java
@@ -1,12 +1,16 @@
 package at.tugraz.software22.domain.entity;
 
+import java.time.LocalDate;
+
 public class User {
     private String username;
     private String password;
     private String email;
+    private LocalDate birthday;
 
-    public User (String username) {
+    public User (String username, LocalDate birthday) {
         this.username = username;
+        this.birthday = birthday;
     }
 
     public String getUsername() {
@@ -14,4 +18,8 @@ public class User {
     }
 
     public void setUsername(String username) { this.username = username; }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
 }
