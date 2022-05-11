8193f31fd3f4cde0bea99ce844be06ce6c132ded
Michael Baier <baier@student.tugraz.at>
2022-05-11 09:07:23 +0200

    WUFF-3 RED Add testcase to check if the correct User is displayed at the edit user activity [JS, MB]

---
+++ app/src/main/java/at/tugraz/software22/ui/EditProfileActivity.java
@@ -0,0 +1,15 @@
+package at.tugraz.software22.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
