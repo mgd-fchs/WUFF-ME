77e49d025b377f7d6307a73047c8e03460b0b3c1
Felix Schmidt <felix.schmidt@student.tugraz.at>
2022-05-11 10:24:50 +0200

    MAINTENANCE add database reference [Ermal,Magdalena,Felix]

---
+++ app/src/main/java/at/tugraz/software22/Constants.java
@@ -0,0 +1,6 @@
+package at.tugraz.software22;
+
+public class Constants {
+    public static final String DATABASE_URL = "https://wuff-d5f14-default-rtdb.europe-west1.firebasedatabase.app/";
+    public static final String USER_TABLE = "users";
+}
