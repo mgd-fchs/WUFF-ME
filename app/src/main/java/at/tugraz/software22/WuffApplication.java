869cc27239c3275f6ff82c696dc4cf48e06e8335
Felix Schmidt <felix.schmidt@student.tugraz.at>
2022-05-13 12:06:58 +0200

    Revert "WUFF-1 RED Basic database setup with test [MH,FS]"
    This reverts commit a9f3054877b473e5a5bfd4deed9dff75f4fff839.

--- app/src/main/java/at/tugraz/software22/WuffApplication.java
+++
@@ -1,6 +0,0 @@
-package at.tugraz.software22;
-
-import android.app.Application;
-
-public class WuffApplication extends Application {
-}
--- app/src/main/java/at/tugraz/software22/domain/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package at.tugraz.software22.domain.repository;
-
-import at.tugraz.software22.domain.entity.User;
-
-public interface UserRepository {
-    boolean registerUser(User user);
-}
