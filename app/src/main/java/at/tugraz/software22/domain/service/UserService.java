2d88b761ff6d7091bb9d7f4783018b98e6713976
Jakob Stanta <jakob.stanta@student.tugraz.at>
2022-05-18 11:54:36 +0200

    WUFF-3 GREEN Implement UserService::updateUser [JS,MB]

--- app/src/main/java/at/tugraz/software22/domain/service/UserService.java
+++ app/src/main/java/at/tugraz/software22/domain/service/UserService.java
@@ -1,17 +1,24 @@
 package at.tugraz.software22.domain.service;
 
-import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import at.tugraz.software22.Constants;
 import at.tugraz.software22.domain.entity.User;
 import at.tugraz.software22.domain.repository.UserRepository;
 
 public class UserService implements UserRepository {
-    DatabaseReference databaseReference;
+    final FirebaseDatabase database;
+    private FirebaseAuth mAuth;
 
-    public UserService() {
-        databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl(Constants.DATABASE_URL);
+    User loggedInUser;
+
+    public UserService(FirebaseDatabase database, FirebaseAuth mAuth) {
+        this.database = database;
+        this.mAuth = mAuth;
     }
 
     @Override
@@ -21,11 +28,14 @@ public class UserService implements UserRepository {
 
     @Override
     public User getLoggedInUser() {
-        return null;
+        return loggedInUser;
     }
 
     @Override
     public void updateUser(User user) {
-
+        Map<String, Object> users = new HashMap<>();
+        users.put(mAuth.getCurrentUser().getUid(), user);
+        database.getReference().child(Constants.USER_TABLE).updateChildren(users);
+        loggedInUser = user;
     }
 }
--- app/src/test/java/at/tugraz/software22/domain/service/UserServiceTest.java
+++ app/src/test/java/at/tugraz/software22/domain/service/UserServiceTest.java
@@ -1,6 +1,9 @@
 package at.tugraz.software22.domain.service;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -8,6 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.invocation.MockHandler;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.LocalDate;
@@ -21,14 +25,28 @@ public class UserServiceTest {
 
     private UserService userService;
 
+    @Mock
+    private FirebaseDatabase database;
+    @Mock
+    private FirebaseAuth auth;
+
     @Before
     public void setUp(){
-        userService = new UserService();
+        userService = new UserService(database, auth);
     }
 
     @Test
     public void givenDatabaseWithOneUser_whenUserUpdated_thenLoggedInUserIsUpdated(){
+        String uid = "h6MVwVQvlZOy6FeJh9us88aTNvu1";
         User updatedUser = new User("Testuser", LocalDate.now(), "Developer");
+        FirebaseUser firebaseUser = Mockito.mock(FirebaseUser.class);
+        Mockito.when(firebaseUser.getUid()).thenReturn(uid);
+        Mockito.when(auth.getCurrentUser()).thenReturn(firebaseUser);
+
+        DatabaseReference reference = Mockito.mock(DatabaseReference.class);
+        DatabaseReference childReference = Mockito.mock(DatabaseReference.class);
+        Mockito.when(reference.child(Mockito.any())).thenReturn(childReference);
+        Mockito.when(database.getReference()).thenReturn(reference);
 
         userService.updateUser(updatedUser);
         User loggedInUser = userService.getLoggedInUser();
@@ -36,6 +54,7 @@ public class UserServiceTest {
         Assert.assertEquals(updatedUser.getUsername(), loggedInUser.getUsername());
         Assert.assertEquals(updatedUser.getBirthday(), loggedInUser.getBirthday());
         Assert.assertEquals(updatedUser.getJob(), loggedInUser.getJob());
+        Mockito.verify(childReference).updateChildren(Mockito.argThat(arg -> arg.containsKey(uid)));
     }
 
 }
