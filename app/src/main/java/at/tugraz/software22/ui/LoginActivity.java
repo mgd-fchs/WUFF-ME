d5fd961c7609f20f466ae85c80c1594de28dbf2f
Magdalena Hinterkoerner <m.hinterkoerner@student.tugraz.at>
2022-05-25 11:52:03 +0200

    WUFF-4 GREEN Display upload button only on register [MB, MH]

--- app/src/main/java/at/tugraz/software22/ui/LoginActivity.java
+++ app/src/main/java/at/tugraz/software22/ui/LoginActivity.java
@@ -1,10 +1,13 @@
 package at.tugraz.software22.ui;
 
+import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -12,12 +15,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import at.tugraz.software22.R;
-import at.tugraz.software22.domain.entity.User;
-import at.tugraz.software22.domain.enums.UserState;
+import at.tugraz.software22.domain.entity.Users;
 import at.tugraz.software22.ui.viewmodel.UserViewModel;
 
 public class LoginActivity extends AppCompatActivity {
+    private static Application wuffApp;
     private UserViewModel userViewModel;
+    public static final String INTENT_USERNAME = "";
     private static String username;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,79 +32,76 @@ public class LoginActivity extends AppCompatActivity {
         EditText passwordInput = (EditText)findViewById(R.id.password);
         EditText usernameInput = (EditText)findViewById(R.id.username);
         Switch toggleBtn = (Switch) findViewById(R.id.toggle_register);
+        ImageView uploadImage = (ImageView)findViewById(R.id.image_button_add_profile_picture);
 
         userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+        wuffApp = getApplication();
 
         Button loginBtn = (Button)findViewById(R.id.login_btn);
 
-        toggleBtn.setOnCheckedChangeListener((buttonView, isChecked) -> {
+        toggleBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
-            if (isChecked){
-                usernameInput.setVisibility(View.VISIBLE);
-                loginBtn.setText("REGISTER");
-            } else {
-                usernameInput.setVisibility(View.INVISIBLE);
-                loginBtn.setText("LOG IN");
-            }
-        });
-
-        loginBtn.setOnClickListener(v -> {
-
-            if (usernameInput.getVisibility() == View.VISIBLE) {
-                username = usernameInput.getText().toString();
-
-                if (username.isEmpty()) {
-                    usernameInput.setError("Please enter a username!");
-                    usernameInput.requestFocus();
-                    return;
+                if (isChecked){
+                    usernameInput.setVisibility(View.VISIBLE);
+                    uploadImage.setVisibility(View.VISIBLE);
+                    loginBtn.setText("REGISTER");
+                } else {
+                    usernameInput.setVisibility(View.INVISIBLE);
+                    uploadImage.setVisibility(View.GONE);
+                    loginBtn.setText("LOG IN");
                 }
             }
-
-            String email = emailInput.getText().toString();
-            String password = passwordInput.getText().toString();
-
-            if (email.isEmpty()) {
-                emailInput.setError("Please enter an email address!");
-                emailInput.requestFocus();
-                return;
-            }
-            if (password.isEmpty()) {
-                passwordInput.setError("Please enter a password!");
-                passwordInput.requestFocus();
-                return;
-            }
-
-            if (password.length() < 6){
-                passwordInput.setError("Password needs to contain at least 6 characters!");
-                passwordInput.requestFocus();
-                return;
-            }
-
-            if (!email.matches("^(.+)@(\\S+)$")) {
-                emailInput.setError("This email address is invalid!");
-                emailInput.requestFocus();
-                return;
-            }
-
-            User users = new User(username, password, email);
-
-            if (usernameInput.getVisibility() == View.VISIBLE) {
-                userViewModel.registerUser(users);
-            } else {
-                userViewModel.loginUser(users);
-            }
         });
 
-        userViewModel.getUserService().getUserState().observe(this, result -> {
-            if (result == UserState.LOGGED_IN_FROM_REGISTRATION){
-                Intent intent = new Intent(LoginActivity.this, UsertypeSelectionActivity.class);
-                startActivity(intent);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (usernameInput.getVisibility() == View.VISIBLE) {
+                    username = usernameInput.getText().toString();
+
+                    if (username.isEmpty()) {
+                        usernameInput.setError("Please enter a username!");
+                        usernameInput.requestFocus();
+                    }
+                }
+
+                String email = emailInput.getText().toString();
+                String password = passwordInput.getText().toString();
+
+                if (email.isEmpty()) {
+                    emailInput.setError("Please enter an email address!");
+                    emailInput.requestFocus();
+                }
+                if (password.isEmpty()) {
+                    passwordInput.setError("Please enter a password!");
+                    passwordInput.requestFocus();
+                }
+
+                if (password.length() < 6){
+                    passwordInput.setError("Password needs to contain at least 6 characters!");
+                    passwordInput.requestFocus();
+                }
+
+                if (!email.matches("^(.+)@(\\S+)$")) {
+                    emailInput.setError("This email address is invalid!");
+                    emailInput.requestFocus();
+                }
+
+                Users users = new Users(username, password, email);
+
+                userViewModel.registerUser(users);
             }
-            else if (result == UserState.LOGGED_IN_FROM_LOGIN) {
+
+        });
+
+        userViewModel.getUserService().getRegistrationSuccess().observe(this, result -> {
+            if (result){
+
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
-            }
-            else {
+            } else {
                 Toast toast;
                 toast = Toast.makeText(getApplicationContext(), "Login/Registration unsuccessful!", Toast.LENGTH_LONG);
                 toast.show();
