b00d97029a62c719ebb7ad0430180fa1a0ab70fb
Michael Baier <baier@student.tugraz.at>
2022-05-25 11:08:22 +0200

    WUFF-3 REFACTOR Extract constant string value for profile picture storage [MB, MH]

--- app/src/main/java/at/tugraz/software22/Constants.java
+++ app/src/main/java/at/tugraz/software22/Constants.java
@@ -3,4 +3,5 @@ package at.tugraz.software22;
 public class Constants {
     public static final String DATABASE_URL = "https://wuff-d5f14-default-rtdb.europe-west1.firebasedatabase.app/";
     public static final String USER_TABLE = "users";
+    public static final String USER_PROFILE_PICTURE_STORAGE_LOCATION = "profile_picture";
 }
