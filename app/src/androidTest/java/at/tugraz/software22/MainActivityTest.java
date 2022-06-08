6b85453006b69b17ebda09f09a368f73854bc2ac
Florian Buchacher <florian.buchacher@student.tugraz.at>
2022-06-08 11:23:09 +0200

    WUFF-11 GREEN Add navigation buttons and update main activity test [FB,EG]

--- app/src/androidTest/java/at/tugraz/software22/MainActivityTest.java
+++ app/src/androidTest/java/at/tugraz/software22/MainActivityTest.java
@@ -9,9 +9,7 @@ import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -43,6 +41,4 @@ public class MainActivityTest {
         Espresso.onView(ViewMatchers.withText(R.string.button_matches)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
         Espresso.onView(ViewMatchers.withText(R.string.button_messages)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
-
-
 }
