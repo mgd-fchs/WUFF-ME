4750f87b1f3c817e7c8a8846f44a020648aacd1b
Florian Buchacher <florian.buchacher@student.tugraz.at>
2022-06-08 11:53:14 +0200

    WUFF-11 RED Add navigation button tests [FB,EG]

--- app/src/androidTest/java/at/tugraz/software22/MainActivityTest.java
+++ app/src/androidTest/java/at/tugraz/software22/MainActivityTest.java
@@ -1,5 +1,8 @@
 package at.tugraz.software22;
 
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+
 import android.content.Context;
 import android.content.res.Resources;
 
@@ -7,13 +10,16 @@ import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import at.tugraz.software22.ui.ChatActivity;
 import at.tugraz.software22.ui.LoginActivity;
+import at.tugraz.software22.ui.MatchesActivity;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -40,5 +46,55 @@ public class MainActivityTest {
         Espresso.onView(ViewMatchers.withText(R.string.button_swiping)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
         Espresso.onView(ViewMatchers.withText(R.string.button_matches)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
         Espresso.onView(ViewMatchers.withText(R.string.button_messages)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+
+    @Test
+    public void givenMainActivity_whenMatchesButtonIsPressed_thenMatchesWindowPresent() throws InterruptedException {
+
+        Intents.init();
+        ActivityScenario.launch(LoginActivity.class);
+
+        Espresso.onView(ViewMatchers.withId(R.id.toggle_register)).perform(ViewActions.click());
+
+        Espresso.onView(ViewMatchers.withId(R.id.username)).perform(ViewActions.clearText(), ViewActions.typeText("user"));
+        Espresso.onView(ViewMatchers.withId(R.id.email)).perform(ViewActions.clearText(), ViewActions.typeText("email@yahoo.at" + Math.random() * 1000000));
+        Espresso.onView(ViewMatchers.withId(R.id.password)).perform(ViewActions.clearText(), ViewActions.typeText("1234567"), ViewActions.closeSoftKeyboard());
+        Espresso.onView(ViewMatchers.withId(R.id.login_btn)).perform(ViewActions.click());
+
+        Thread.sleep(2000);
+        Espresso.onView(ViewMatchers.withId(R.id.checkBoxOwner)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.buttonSelectUsertype)).perform(ViewActions.click());
+        Thread.sleep(2000);
+
+        Espresso.onView(ViewMatchers.withId(R.id.buttonMatches)).perform(ViewActions.click());
+
+        Thread.sleep(2000);
+
+        intended(hasComponent(MatchesActivity.class.getName()));
+    }
+
+    @Test
+    public void givenMainActivity_whenChatButtonIsPressed_thenChatWindowPresent() throws InterruptedException {
+
+        Intents.init();
+        ActivityScenario.launch(LoginActivity.class);
+
+        Espresso.onView(ViewMatchers.withId(R.id.toggle_register)).perform(ViewActions.click());
+
+        Espresso.onView(ViewMatchers.withId(R.id.username)).perform(ViewActions.clearText(), ViewActions.typeText("user"));
+        Espresso.onView(ViewMatchers.withId(R.id.email)).perform(ViewActions.clearText(), ViewActions.typeText("email@yahoo.at" + Math.random() * 1000000));
+        Espresso.onView(ViewMatchers.withId(R.id.password)).perform(ViewActions.clearText(), ViewActions.typeText("1234567"), ViewActions.closeSoftKeyboard());
+        Espresso.onView(ViewMatchers.withId(R.id.login_btn)).perform(ViewActions.click());
+
+        Thread.sleep(2000);
+        Espresso.onView(ViewMatchers.withId(R.id.checkBoxOwner)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.buttonSelectUsertype)).perform(ViewActions.click());
+        Thread.sleep(2000);
+
+        Espresso.onView(ViewMatchers.withId(R.id.buttonMatches)).perform(ViewActions.click());
+
+        Thread.sleep(2000);
+
+        intended(hasComponent(ChatActivity.class.getName()));
     }
 }
---
+++ app/src/main/java/at/tugraz/software22/ui/ChatActivity.java
@@ -0,0 +1,15 @@
+package at.tugraz.software22.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import at.tugraz.software22.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
