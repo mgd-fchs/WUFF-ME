e2f1024d37a909ee13c28377dfbc7edddce2cc7c
Jakob Stanta <jakob.stanta@student.tugraz.at>
2022-06-08 11:13:07 +0200

    WUFF-6 GREEN Implement MatcherService to get next interesting user as a Searcher [FS,JS]

--- app/src/main/java/at/tugraz/software22/domain/service/MatcherService.java
+++ app/src/main/java/at/tugraz/software22/domain/service/MatcherService.java
@@ -1,21 +1,62 @@
 package at.tugraz.software22.domain.service;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import at.tugraz.software22.Constants;
 import at.tugraz.software22.domain.entity.User;
+import at.tugraz.software22.domain.enums.UserType;
 import at.tugraz.software22.domain.repository.UserRepository;
 
 public class MatcherService {
-    List<String> interestingProfiles = new ArrayList<>();
+    List<User> interestingProfiles = new ArrayList<>();
+    private final UserRepository userService;
+    private final FirebaseDatabase database;
+    MutableLiveData<User> user = new MutableLiveData<>();
 
-
-    public MatcherService(UserRepository userService) {
-
+    public MatcherService(FirebaseDatabase database, UserRepository userService) {
+        this.userService = userService;
+        this.database = database;
     }
 
-    public User getNextInterestingProfile() {
+    public LiveData<User> getNextInterestingProfile() {
+        if (interestingProfiles.size() <= 1) {
+            fetchNewProfiles(userService.getLoggedInUser().getType());
+        }
+        else {
+            user.postValue(interestingProfiles.remove(0));
+        }
+        return user;
+    }
 
-        return null;
+    private void fetchNewProfiles(UserType ownType) {
+       database.getReference().child(Constants.USER_TABLE).get().addOnSuccessListener(dataSnapshot -> {
+           for (DataSnapshot child : dataSnapshot.getChildren()) {
+               User user = child.getValue(User.class);
+               switch (ownType) {
+                   case SEARCHER:
+                       if (user.getType() == UserType.OWNER || user.getType() == UserType.BOTH) { // TODO filter already swiped
+                           interestingProfiles.add(user);
+                       }
+                       break;
+               }
+           }
+           if (!interestingProfiles.isEmpty()) {
+               user.postValue(interestingProfiles.remove(0));
+           }
+       });
     }
 }
--- app/src/test/java/at/tugraz/software22/domain/service/MatcherServiceTest.java
+++ app/src/test/java/at/tugraz/software22/domain/service/MatcherServiceTest.java
@@ -1,21 +1,32 @@
 package at.tugraz.software22.domain.service;
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 
+import at.tugraz.software22.Constants;
 import at.tugraz.software22.WuffApplication;
 import at.tugraz.software22.domain.entity.User;
 import at.tugraz.software22.domain.enums.UserType;
@@ -24,12 +35,16 @@ import at.tugraz.software22.ui.viewmodel.UserViewModel;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MatcherServiceTest {
+    @Rule
+    public final InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
     @Mock
     private WuffApplication applicationMock;
 
     @Mock
     private FirebaseStorage firebaseStorage;
 
+    @Mock
+    private FirebaseDatabase firebaseDatabase;
 
 
     @Mock
@@ -41,7 +56,7 @@ public class MatcherServiceTest {
 
     @Before
     public void setUp() {
-        matcherService = new MatcherService(userService);
+        matcherService = new MatcherService(firebaseDatabase, userService);
 
     }
 
@@ -50,8 +65,28 @@ public class MatcherServiceTest {
         User testUser = new User("Testuser", "123456", "Developer");
         testUser.setType(UserType.SEARCHER);
         Mockito.when(userService.getLoggedInUser()).thenReturn(testUser);
-        User userResult = matcherService.getNextInterestingProfile();
-        Assert.assertTrue(checkUserType(UserType.OWNER, userResult.getType()));
+
+        DatabaseReference reference = Mockito.mock(DatabaseReference.class);
+        DatabaseReference childrenReference = Mockito.mock(DatabaseReference.class);
+        Task<DataSnapshot> dataSnapshotTask = Mockito.mock(Task.class);
+        ArgumentCaptor<OnSuccessListener<DataSnapshot>> successListener = ArgumentCaptor.forClass(OnSuccessListener.class);
+        Mockito.when(childrenReference.get()).thenReturn(dataSnapshotTask);
+        Mockito.when(reference.child(Constants.USER_TABLE)).thenReturn(childrenReference);
+        Mockito.when(firebaseDatabase.getReference()).thenReturn(reference);
+
+        DataSnapshot userData = Mockito.mock(DataSnapshot.class);
+        DataSnapshot userSnapshot = Mockito.mock(DataSnapshot.class);
+        User user = new User();
+        user.setType(UserType.OWNER);
+        Mockito.when(userSnapshot.getValue(User.class)).thenReturn(user);
+        Iterable<DataSnapshot> children = List.of(userSnapshot);
+        Mockito.when(userData.getChildren()).thenReturn(children);
+
+
+        LiveData<User> userResult = matcherService.getNextInterestingProfile();
+        userResult.observeForever(u -> Assert.assertTrue(checkUserType(UserType.OWNER, u.getType())));
+        Mockito.verify(dataSnapshotTask).addOnSuccessListener(successListener.capture());
+        successListener.getValue().onSuccess(userData);
     }
 
     Boolean checkUserType(UserType expType, UserType givenType) {
